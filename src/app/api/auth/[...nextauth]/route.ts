79f7137946c72fd779f559bb627e0db7b3f2f616
Håkon <haakonfs@hotmail.com>
2023-06-11 22:19:32 +0200

    prisma setup with user auth flow. password bcrypt, login api,

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,35 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-import prisma from "../../../libs/prismadb";
+import prisma from "../../../../lib/prisma";
 import { NextAuthOptions } from "next-auth";
 
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      name: "Sing in",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "hello@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = { id: "1", name: "J Smith", email: "test@test.com" };
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
@@ -16,6 +37,9 @@ const authOptions: NextAuthOptions = {
     // ...add more providers here
   ],
   debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
 };
 
 const handler = NextAuth(authOptions);
