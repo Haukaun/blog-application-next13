d779439f5feaae20b856d9102d172fca137411b4
Håkon <haakonfs@hotmail.com>
2023-06-15 15:18:17 +0200

    trashcan icon

--- src/components/Blogs/Admin/BlogCardLong.tsx
+++ src/components/Blogs/Admin/BlogCardLong.tsx
@@ -57,17 +57,10 @@ const BlogCardLong: React.FC<BlogCardLongProps> = ({
           <button onClick={deleteBlogPost} className="btn btn-error">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
+              height="1em"
+              viewBox="0 0 448 512"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 2a9 9 0 100 18 9 9 0 000-18zm0 14a5 5 0 100-10 5 5 0 000 10z"
-              />
+              <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
             </svg>
           </button>
           <a href={"/" + id} className="btn btn-primary">
