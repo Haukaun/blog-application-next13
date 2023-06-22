65cb009ba640ac08c74eff9696ed17220737b174
Håkon <haakonfs@hotmail.com>
2023-06-23 01:26:44 +0200

    added page for items

--- src/app/admin/blogItems/[slug]/page.tsx
+++ src/app/admin/blogItems/[slug]/page.tsx
@@ -3,11 +3,12 @@
 import { useSession } from "next-auth/react";
 import React from "react";
 
-export default function page() {
+export default function page({}) {
   const { data: session } = useSession();
   if (session?.user.role === "ADMIN") {
     return (
       <div className="flex justify-center items-center flex-col gap-3 w-full">
+        {/*Make list comp and item card comp*/}
         <h1>Blog Items</h1>
       </div>
     );
