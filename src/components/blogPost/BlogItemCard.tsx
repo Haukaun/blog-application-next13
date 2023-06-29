dc4720ca166046954d176f93a2481bd28aceda96
Håkon <haakonfs@hotmail.com>
2023-06-29 23:15:05 +0200

    blogitemcard daisy update

--- src/components/blogPost/BlogItemCard.tsx
+++ src/components/blogPost/BlogItemCard.tsx
@@ -7,20 +7,20 @@ interface BlogPostItemProps {
 export default function BlogItemCard({ blogPostItem }: BlogPostItemProps) {
   return (
     <div>
-      <h1 className="mt-6 text-4xl font-bold">{blogPostItem.title}</h1>
-      <div className="border p-10 mt-2">
-        <div className="flex justify-around flex-wrap">
-          <div className="relative">
-            <img className="h-28" src="/testimage.jpeg" alt="" />
-            <div className="absolute left-0 bottom-[-2]">image name</div>
-          </div>
-          <div className="m-10">
-            <h1 className="text-2xl font-bold">{blogPostItem.subTitle}</h1>
-            <p className="text-sm max-w-xs">{blogPostItem.subContent}</p>
+      <h1 className="my-6 text-4xl font-bold">{blogPostItem.title}</h1>
+      <div className="card card-side bg-base-100 shadow-xl border">
+        <figure>
+          <img src="/testimage.jpeg" alt="Movie" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{blogPostItem.subTitle}</h2>
+          <p>{blogPostItem.subContent}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Watch</button>
           </div>
         </div>
       </div>
-      <p className="mt-2">{blogPostItem.content}</p>
+      <p className="my-10">{blogPostItem.content}</p>
     </div>
   );
 }
